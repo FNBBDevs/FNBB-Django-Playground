491dc3891277b5d8580b5fff0dc24057724795c2
Ethan Christensen <ethanlchristensen@outlook.com>
2023-05-29 15:11:10 -0500

    updated profile page. Can now update your own profile and have a profile image

--- users/views.py
+++ users/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.models import User
 from blog.models import Post
-from hashlib import sha1
 
 
 def register(request):
@@ -26,11 +25,28 @@ def register(request):
 
 @login_required
 def profile(request):
-        you = User.objects.get(username=request.user)
-        your_posts = [post for post in Post.objects.all() if post.author==you.username]
-        context = {
-            'you': you,
-            'posts': your_posts,
-            'title': 'Profile'
-        }
+        if request.method == 'POST':
+            user_update_form = UserUpdateForm(request.POST, instance=request.user)
+            profile_update_form = ProfileUpdateForm(request.POST, request.FILES,
+                                                    instance=request.user.profile)
+            
+            if user_update_form.is_valid() and profile_update_form.is_valid():
+                user_update_form.save()
+                profile_update_form.save()
+                messages.success(request, "Your profile has been updated successfully.")
+                return redirect('profile')
+
+
+        else:
+            user_update_form = UserUpdateForm(instance=request.user)
+            profile_update_form = ProfileUpdateForm(instance=request.user.profile)
+            you = User.objects.get(username=request.user)
+            your_posts = [post for post in Post.objects.all() if post.author==you.username]
+            context = {
+                'you': you,
+                'posts': your_posts,
+                'title': 'Profile',
+                'user_update_form': user_update_form,
+                'profile_update_form': profile_update_form
+            }
         return render(request, 'users/profile.html', context)
